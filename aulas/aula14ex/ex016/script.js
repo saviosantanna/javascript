b2606afbad2d58299c31ee8d7dbd2b5cb89889e1
saviosantanna <saviosantannadesa@gmail.com>
2022-03-26 21:34:53 -0300

    ex016

--- aulas/aula14ex/ex016/script.js
+++ aulas/aula14ex/ex016/script.js
@@ -1,26 +1,40 @@
-// emoji dedo &#128073 // bandeira &#127937
+// emoji dedo U+1F449 // bandeira U+1F3C1	
 
 function contar(){
-    var inicio = Number(window.document.getElementById('txtinicio').value)
-    var fim = Number(window.document.getElementById('txtfim').value)
-    var passo = Number(window.document.getElementById('txtpasso').value)
+    var inicio =window.document.getElementById('txtinicio')
+    var fim = window.document.getElementById('txtfim')
+    var passo = window.document.getElementById('txtpasso')
     var res = window.document.getElementById('res')
-    /*window.alert(`Valores digitados:\nValor de Início: ${inicio}\nValor Final: ${fim}\nPasso: ${passo}`)*/
     
-    if (inicio == "" || fim == "" || passo == ""){
-        window.alert(`Favor preencher todos os campos`)
-    } else if (fim < inicio){
-            window.alert(`O valor de Início não pode ser maior que o valor fim.`)
-        } else { 
-            var contagem= `Contando: \n`
-            for (var i = inicio; i <= fim; i = i + passo){
-                if (i==inicio){
-                    contagem = i
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert(`[ERRO!] Favor preencher todos os campos, com passo maior que 0.`)
+    } else {
+        inicio = Number (inicio.value)
+        fim = Number (fim.value)
+        passo = Number (passo.value)
+            if (passo == 0 || passo < 0){
+                window.alert(`[ERRO] Passo deve ser maior que 0!`)
+            } else {
+                var contagem= `Contando: \n`
+                if (inicio < fim){
+                    for (var i = inicio; i <= fim; i = i + passo){
+                        if (i==inicio){
+                            contagem += i
+                        } else {
+                            contagem = `${contagem} \u{1F449} ${i}`
+                        }                
+                    }
                 } else {
-                    contagem = `${contagem} > ${i}`
-                }                
-            }
-            contagem = `${contagem} > bandeira`            
-            res.innerHTML = `${contagem}`
-        }        
+                    for (var i = inicio; i >= fim; i = i - passo){
+                        if (i==inicio){
+                            contagem += i
+                        } else {
+                            contagem = `${contagem} \u{1F449} ${i}`
+                        }                
+                    }
+                }
+                contagem = `${contagem} > \u{1F3C1}`            
+                res.innerHTML = `${contagem}`
+                }
+            }    
 }
